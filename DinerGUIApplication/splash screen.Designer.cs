ee266927b7fdc290ef7929055b3f2de6c1e85c3e
Emmjc <150797375+Emmjc@users.noreply.github.com>
2023-11-14 18:51:17 +0800

    Add files via upload

---
+++ DinerGUIApplication/splash screen.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DinerGUIApplication
+{
+    partial class splash_screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(splash_screen));
+            pictureBox1 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            lblLoading = new Label();
+            lblNum = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(680, 362);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            progressBar1.ForeColor = Color.SteelBlue;
+            progressBar1.Location = new Point(0, 350);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(680, 12);
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // lblLoading
+            // 
+            lblLoading.AutoSize = true;
+            lblLoading.BackColor = SystemColors.ActiveCaptionText;
+            lblLoading.Font = new Font("Arial Narrow", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblLoading.ForeColor = SystemColors.ButtonFace;
+            lblLoading.Location = new Point(0, 327);
+            lblLoading.Name = "lblLoading";
+            lblLoading.Size = new Size(43, 20);
+            lblLoading.TabIndex = 2;
+            lblLoading.Text = "label1";
+            // 
+            // lblNum
+            // 
+            lblNum.AutoSize = true;
+            lblNum.BackColor = SystemColors.ActiveCaptionText;
+            lblNum.Font = new Font("Arial Narrow", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblNum.ForeColor = SystemColors.ButtonFace;
+            lblNum.Location = new Point(635, 327);
+            lblNum.Name = "lblNum";
+            lblNum.Size = new Size(43, 20);
+            lblNum.TabIndex = 3;
+            lblNum.Text = "label1";
+            // 
+            // splash_screen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(680, 362);
+            Controls.Add(lblNum);
+            Controls.Add(lblLoading);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "splash_screen";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "splash_screen";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private Label lblLoading;
+        private Label lblNum;
+    }
+}
---
+++ DinerGUIApplication/splash screen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinerGUIApplication
+{
+    public partial class splash_screen : Form
+    {
+        public splash_screen()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1);
+
+            if (progressBar1.Value <= 25)
+            {
+                lblLoading.Text = "Gathering data, please wait...";
+            }
+            else if (progressBar1.Value <= 50)
+            {
+                lblLoading.Text = "Preparing your food...";
+            }
+            else if (progressBar1.Value <= 75)
+            {
+                lblLoading.Text = "Ready your money...";
+            }
+            else
+            {
+                lblLoading.Text = "Ready your money and tummy!";
+            }
+
+            // Update percentage label text
+            lblNum.Text = progressBar1.Value.ToString() + "%";
+
+            if (progressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+                Form1 form = new Form1();
+                form.Show();
+                this.Hide();
+            }
+        }
+    }
+}
